09beefc1ff285f5640badc5e7776710fe76d2966
gwendvr <gwendevriendtgd@gmail.com>
2022-04-12 15:16:18 +0200

    initialisation

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class AppFixtures extends Fixture
+{
+    private UserPasswordHasherInterface $hasher;
+
+    public function __construct(UserPasswordHasherInterface $hasher)
+    {
+        $this->hasher = $hasher;
+    }
+
+    public function load(ObjectManager $manager): void
+    {
+        $user = new User();
+        $user->setName('Gwen');
+        $user->setLastName('Devriendt');
+        $password = $this->hasher->hashPassword($user, 'ge4z55G5YM');
+        $user->setPassword($password);
+        $user->setEmail('gwenaelle.devriendt@gmail.com');
+        $user->setRoles(['ROLE_ADMIN']);
+
+        $manager->persist($user);
+        $manager->flush();
+    }
+}
