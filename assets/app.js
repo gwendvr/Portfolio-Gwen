6684322b528c92442f495edec2025a86fb61f9e1
gwendvr <gwendevriendtgd@gmail.com>
2022-04-19 17:36:38 +0200

    change scss

--- assets/app.js
+++ assets/app.js
@@ -7,6 +7,9 @@
 
 // any CSS you import will output into a single css file (app.scss in this case)
 import './styles/app.scss';
+import './styles/header.scss';
+import './styles/footer.scss';
+import './styles/store.scss';
 
 // start the Stimulus application
 import './bootstrap';
