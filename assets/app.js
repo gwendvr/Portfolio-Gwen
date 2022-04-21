1d83819cf15117798b33a1784967a0cab8c6fc31
gwendvr <gwendevriendtgd@gmail.com>
2022-04-21 17:14:43 +0200

    admin fait

--- assets/app.js
+++ assets/app.js
@@ -12,6 +12,7 @@ import './styles/footer.scss';
 import './styles/store.scss';
 import './styles/index.scss';
 import './styles/table.scss';
+import './styles/admin.scss';
 
 // start the Stimulus application
 import './bootstrap';
