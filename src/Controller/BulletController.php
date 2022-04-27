a49848da6be59d481cb358bd3a5089a7239b4d07
gwendvr <gwendevriendtgd@gmail.com>
2022-04-27 09:10:02 +0200

    add reset button

--- src/Controller/BulletController.php
+++ src/Controller/BulletController.php
@@ -3,8 +3,11 @@
 namespace App\Controller;
 
 use App\Entity\Tasks;
+use App\Entity\WeekNumber;
+use App\Form\NumberType;
 use App\Form\TaskType;
 use App\Repository\TasksRepository;
+use App\Repository\WeekNumberRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -13,21 +16,25 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class BulletController extends AbstractController
 {
-    public function __construct(EntityManagerInterface $em, TasksRepository $repoTask)
+    public function __construct(EntityManagerInterface $em, TasksRepository $repoTask, WeekNumberRepository $repoNumber)
     {
         $this->em = $em;
         $this->repoTask = $repoTask;
+        $this->repoNumber = $repoNumber;
     }
 
     #[Route('/bullet', name: 'app_bullet')]
     public function index(): Response
     {
         $tasks = $this->repoTask->findAll();
+        $numbers = $this->repoNumber->findAll();
         return $this->render('bullet/index.html.twig', [
             'tasks'=> $tasks,
+            'numbers'=> $numbers
         ]);
     }
 
+    /****************** TASKS ******************/
     #[Route('/bullet/createTask', name: 'create_task')]
     public function createTask(Request $request) : Response
     {
@@ -42,7 +49,7 @@ class BulletController extends AbstractController
 
             return $this->redirectToRoute('app_bullet');
         }
-        return $this->render("bullet/CreateTask.html.twig", [
+        return $this->render("bullet/tasks/CreateTask.html.twig", [
             'form' => $form->createView()
         ]);
     }
@@ -61,7 +68,7 @@ class BulletController extends AbstractController
 
             return $this->redirectToRoute('app_bullet');
         }
-        return $this->render("bullet/EditTask.html.twig", [
+        return $this->render("bullet/tasks/EditTask.html.twig", [
             'form' => $form->createView(),
             'task' => $task
         ]);
@@ -77,6 +84,50 @@ class BulletController extends AbstractController
             $this->addFlash('Success', 'La tâche a été supprimé !');
         }
 
+        return $this->redirectToRoute('app_bullet');
+    }
+
+    /****************** WEEK NUMBER ******************/
+    #[Route('/bullet/createNumber', name: 'create_Number')]
+    public function createNumber(Request $request) : Response
+    {
+        $number = new WeekNumber();
+        $form = $this->createForm(NumberType::class, $number);
+        $form->handleRequest($request);
+        
+        if ($form->isSubmitted() && $form->isValid())
+        {
+            $this->em->persist($number);
+            $this->em->flush();
+
+            return $this->redirectToRoute('app_bullet');
+        }
+        return $this->render("bullet/number/CreateNumber.html.twig", [
+            'form' => $form->createView()
+        ]);
+    }
+
+    /****************** RESET BUTTON ******************/
+
+    #[Route('/bullet/delete', name: 'bullet_reset')]
+    public function resetTable(Request $request)
+    {
+        $taskTable = $this->repoTask->findAll();
+        $weekTable = $this->repoNumber->findAll();
+
+        if($this->isCsrfTokenValid('deleteBullet', $request->get('_token')))
+        {
+            foreach ($taskTable as $task) {
+                $this->em->remove($task);
+                $this->em->flush();
+            }
+
+            foreach ($weekTable as $week) {
+                $this->em->remove($week);
+                $this->em->flush();
+            }
+        }
+        
         return $this->redirectToRoute('app_bullet');
     }
 }
