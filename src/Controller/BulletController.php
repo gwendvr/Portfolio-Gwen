a07ca92793c2b4dd89ab6e4503a99584026a37b6
gwendvr <gwendevriendtgd@gmail.com>
2022-04-27 08:28:05 +0200

    modif and delete task

--- src/Controller/BulletController.php
+++ src/Controller/BulletController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Tasks;
 use App\Form\TaskType;
+use App\Repository\TasksRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -12,19 +13,22 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class BulletController extends AbstractController
 {
-    public function __construct(EntityManagerInterface $em)
+    public function __construct(EntityManagerInterface $em, TasksRepository $repoTask)
     {
         $this->em = $em;
+        $this->repoTask = $repoTask;
     }
 
     #[Route('/bullet', name: 'app_bullet')]
     public function index(): Response
     {
+        $tasks = $this->repoTask->findAll();
         return $this->render('bullet/index.html.twig', [
+            'tasks'=> $tasks,
         ]);
     }
 
-    #[Route('/bullet/createTask', name: 'bullet_create_task')]
+    #[Route('/bullet/createTask', name: 'create_task')]
     public function createTask(Request $request) : Response
     {
         $task = new Tasks();
@@ -41,5 +45,38 @@ class BulletController extends AbstractController
         return $this->render("bullet/CreateTask.html.twig", [
             'form' => $form->createView()
         ]);
+    }
+
+    #[Route('/bullet/editTask/{id}', name: 'edit_task')]
+    public function editTask($id, Request $request) : Response
+    {
+        $task = $this->repoTask->find($id);
+        $form = $this->createForm(TaskType::class, $task);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid())
+        {
+            $this->em->flush();
+            $this->addFlash('Success', 'La tâche a bien été modifiée !');
+
+            return $this->redirectToRoute('app_bullet');
+        }
+        return $this->render("bullet/EditTask.html.twig", [
+            'form' => $form->createView(),
+            'task' => $task
+        ]);
+    }
+
+    #[Route('/bullet/deleteTask/{id}', name: 'delete_task')]
+    public function deleteTask(int $id, Request $request, Tasks $task) : Response
+    {
+        if($this->isCsrfTokenValid('delete'. $task->getId(), $request->get('_token')))
+        {
+            $this->em->remove($task);
+            $this->em->flush();
+            $this->addFlash('Success', 'La tâche a été supprimé !');
+        }
+
+        return $this->redirectToRoute('app_bullet');
     }
 }
