7d816cfff79401c657ceffe1cb01507a5066b40a
OSCOURALED <mathis.vial@bbox.fr>
2022-05-03 09:36:05 +0200

    addclass js

--- assets/js/bullet-valide.js
+++ assets/js/bullet-valide.js
@@ -1,8 +1,17 @@
-function addClass() {
+function goalAddClass() {
     var elem = document.getElementById("form-valide");
-    if (element.classList.contains(className)) {
-        element.classList.remove(className)
+    if (element.classList.contains(goalRestant)) {
+        element.classList.remove(goalRestant)
     } else {
-        element.classList.add(className)
+        element.classList.add(goalValide)
+    }
+}
+
+function shopAddClass() {
+    var elem = document.getElementById("form-valide");
+    if (element.classList.contains(shopRestant)) {
+        element.classList.remove(shopRestant)
+    } else {
+        element.classList.add(shopValide)
     }
 }
