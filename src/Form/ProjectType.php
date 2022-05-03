8ed1fe257a08e8a35599e5ab1f0f3d35ea52199e
gwendvr <gwendevriendtgd@gmail.com>
2022-05-03 15:25:09 +0200

    add competence in project

--- src/Form/ProjectType.php
+++ src/Form/ProjectType.php
@@ -2,7 +2,9 @@
 
 namespace App\Form;
 
+use App\Entity\CompetenceProject;
 use App\Entity\Project;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -17,6 +19,10 @@ class ProjectType extends AbstractType
             ->add('description')
             ->add('image')
             ->add('lien')
+            ->add('competence', EntityType::class, [
+                'class' => CompetenceProject::class,
+                'choice_label' => 'name'
+            ])
             ->add('Enregistrer', SubmitType::class, [
                 'attr' => [
                     'class' => 'btn',
