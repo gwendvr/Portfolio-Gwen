a70a465736cd5fa5c231cc6d0cb890b05f55e2a8
OSCOURALED <mathis.vial@bbox.fr>
2022-05-03 09:30:04 +0200

    correction aclass/iclass

---
+++ assets/js/bullet-valide.js
@@ -0,0 +1,8 @@
+function addClass() {
+    var elem = document.getElementById("form-valide");
+    if (element.classList.contains(className)) {
+        element.classList.remove(className)
+    } else {
+        element.classList.add(className)
+    }
+}
